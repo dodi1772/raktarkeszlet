827c890c2047af3de20231b0df83f954849b14dd
dodi1772 <zsigmond.dorian01@gmail.com>
2025-11-12 09:08:13 +0100

    Fajlkezeles

--- raktarkeszlet/MainWindow.xaml.cs
+++ raktarkeszlet/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO; // <-- szükséges a fájlműveletekhez
 
 namespace raktarkeszlet
 {
@@ -70,6 +71,53 @@ namespace raktarkeszlet
 			else
 			{
 				MessageBox.Show("Kérlek jelölj ki egy elemet a listából.");
+			}
+		}
+		private void MentesFajlbaClick(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				using (StreamWriter sw = new StreamWriter("raktar.txt", false, Encoding.UTF8))
+				{
+					foreach (var item in keszletLista.Items)
+					{
+						sw.WriteLine(item.ToString());
+					}
+				}
+				MessageBox.Show("Mentés sikeres!", "Mentés", MessageBoxButton.OK, MessageBoxImage.Information);
+			}
+			catch (IOException ex)
+			{
+				MessageBox.Show("Hiba mentés közben: " + ex.Message, "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
+		private void BetoltesFajlbolClick(object sender, RoutedEventArgs e)
+		{
+			if (!File.Exists("raktar.txt"))
+			{
+				MessageBox.Show("A raktar.txt fájl nem található.", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+				return;
+			}
+
+			try
+			{
+				keszletLista.Items.Clear();
+				osszDarab = 0;
+				foreach (var sor in File.ReadAllLines("raktar.txt", Encoding.UTF8))
+				{
+					keszletLista.Items.Add(sor);
+					string[] parts = sor.Split(' ');
+					if (parts.Length >= 6 && int.TryParse(parts[5], out int darab))
+					{
+						osszDarab += darab;
+					}
+				}
+				osszdarab.Content = osszDarab;
+				MessageBox.Show("Betöltés sikeres!", "Betöltés", MessageBoxButton.OK, MessageBoxImage.Information);
+			}
+			catch (IOException ex)
+			{
+				MessageBox.Show("Hiba betöltés közben: " + ex.Message, "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
 			}
 		}
 	}
